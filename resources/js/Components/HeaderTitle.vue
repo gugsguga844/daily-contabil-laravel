474ef6b70138be9b326ca0285768a0bddc0419ec
Gustavo Schneider <gusta.schneider123@gmail.com>
2025-09-22 08:38:05 -0300

    Add Header and Sidebar

---
+++ resources/js/Components/HeaderTitle.vue
@@ -0,0 +1,13 @@
+<script setup>
+    defineProps({
+        title: String,
+        subtitle: String
+    })
+</script>
+
+<template>
+    <div class="flex items-center">
+        <h1 class="text-xl font-semibold text-primary">{{ title }}</h1>
+        <p class="text-gray">{{ subtitle }}</p>
+    </div>
+</template>
---
+++ resources/js/Layouts/AppHeader.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import logo from '../../images/logo-png-transparent.png';
+</script>
+
+<template>
+    <header class="bg-[#1E293B] w-full h-16 flex justify-between px-6">
+        <div class="logo h-full">
+            <img :src="logo" alt="Logo" class="object-cover h-16 w-auto">
+        </div>
+        <div class="flex items-center space-x-4">
+            <input type="text" placeholder="Procurar" class="rounded-lg w-full px-4 bg-base-50 placeholder-base-100 text-base-50"></input>
+        </div>
+        <div class="flex items-center space-x-4">
+            <div class="w-8 h-8 bg-accent rounded-full flex items-center justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user w-4 h-4 text-white"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+            </div>
+        </div>
+    </header>
+</template>
