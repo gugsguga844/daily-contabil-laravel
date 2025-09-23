8c1fb44b5b48de6ead8e61539d7505f43dfc5eb8
Gustavo <gusta.schneider123@gmail.com>
2025-09-23 16:43:33 -0300

    Fix PINT error

--- app/Console/Commands/ShowTables.php
+++ app/Console/Commands/ShowTables.php
@@ -8,14 +8,16 @@ use Illuminate\Support\Facades\DB;
 class ShowTables extends Command
 {
     protected $signature = 'tables:show';
+
     protected $description = 'Lista todas as tabelas do banco de dados';
 
     public function handle()
     {
         $tables = DB::select('SELECT name FROM sqlite_master WHERE type="table"');
-        
+
         if (empty($tables)) {
             $this->info('Nenhuma tabela encontrada no banco de dados.');
+
             return;
         }
 
